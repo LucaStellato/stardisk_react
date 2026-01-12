b312f48333954eccbcce101d07b0e28c9c6710a6
LucaStellato <lucastellato435@gmail.com>
2026-01-12 10:27:23 +0100

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,40 +3,25 @@ export default function Footer() {
     return (
         <>
 
-            <footer className='max-vh-100' style={{ backgroundColor: '#fff3e2', height: '' }}>
-                <div className='row'>
-                    <div className='col'>
-                        <img style={{ width: '260px' }} src="/StarDisk.png" />
-                    </div>
-                    <div className='col align-items-center d-flex'>
-                        <ul className='list-unstyled'>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                        </ul>
-                    </div>
-                    <div className='col align-items-center d-flex'>
-                        <ul className='list-unstyled'>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                        </ul>
-                    </div>
-                    <div className='col align-items-center d-flex'>
-                        <ul className='list-unstyled'>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                            <li>features</li>
-                        </ul>
+            <footer className='' style={{ backgroundColor: '#fff3e2', height: '' }}>
+                <div className="container">
+                    <div className='row '>
+
+                        <div className='col-7 align-items-center d-flex ps-5'>
+                            <ul className='list-unstyled'>
+                                <li>features</li>
+                                <li>features</li>
+                                <li>features</li>
+                                <li>features</li>
+                                <li>features</li>
+                                <li>features</li>
+                            </ul>
+                        </div>
+
+
+                        <div className='col-5'>
+                            <img style={{ width: '300px' }} src="/StarDISK-final-logo.png" />
+                        </div>
                     </div>
                 </div>
             </footer>
