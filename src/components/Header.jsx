e1426d889e292b6de72ac844da5787a76affcebe
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-01-13 14:38:07 +0100

    organize css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,26 @@
 import HomePage from '../pages/HomePage'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-yellow">
                 <div className="container-fluid d-flex">
-                    <a className="navbar-brand p-3 " href="#"><img src="/StarDISK-final-logo.png" style={{ height: '75px' }} /></a>
+                    <a className="navbar-brand p-3 " href="#"><img src="/StarDISK-final-logo.png" style={{ height: '60px' }} /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse fs-5 pe-3" id="navbarNav">
-                        <ul className="navbar-nav text-blue ms-auto">
+                        <ul className="navbar-nav text-blue ms-auto fw-bold">
                             <li className="nav-item">
-                                <Link className="nav-link active" to="/">
+                                <NavLink className="nav-link" to="/">
                                     Home
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link active" to="/vinyls">
+                                <NavLink className="nav-link" to="/vinyls">
                                     Catalog
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">About Us</a>
