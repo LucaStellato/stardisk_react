9ea74d2f2fddff367f9dd405b75142e84e4bf5e1
matteovecchio04 <matteovex@gmail.com>
2026-01-13 10:19:50 +0100

    Refactor VinylsPage layout for improved styling and structure

--- src/pages/VinylsPage.jsx
+++ src/pages/VinylsPage.jsx
@@ -16,43 +16,46 @@ export default function VinylsPage() {
     }, [])
 
     return (
-        <div className="container my-5">
-            <h1 className="text-center mb-4">Vinyl Catalog</h1>
+        <div className="bg-lightyellow">
+            <div className="container py-5">
+                <h1 className="text-center mb-4">Vinyl Catalog</h1>
 
-            <div className="row g-4 justify-content-center">
-                {vinyls.map(vinyl => (
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={vinyl.id}>
-                        <div className="vinyl-box-wrapper">
+                <div className="row g-4 justify-content-center">
+                    {vinyls.map(vinyl => (
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={vinyl.id}>
+                            <div className="vinyl-box-wrapper">
 
-                            <div className="vinyl-box">
-                                <img
-                                    src={vinyl.img_url}
-                                    alt={vinyl.name}
-                                    className="vinyl-disc vinyl-disc-top"
-                                />
+                                <div className="vinyl-box">
+                                    <img
+                                        src={vinyl.img_url}
+                                        alt={vinyl.name}
+                                        className="vinyl-disc vinyl-disc-top"
+                                    />
 
-                                <img
-                                    src="/plain-vinyl.webp"
-                                    alt="vinyl disc"
-                                    className="vinyl-disc vinyl-disc-bottom"
-                                />
+                                    <img
+                                        src="/plain-vinyl.webp"
+                                        alt="vinyl disc"
+                                        className="vinyl-disc vinyl-disc-bottom"
+                                    />
 
-                                <div className="vinyl-cover">
-                                    <p className="vinyl-title mb-0">
-                                        {vinyl.name}
-                                    </p>
+                                    <div className="vinyl-cover">
+                                        <p className="vinyl-title mb-0">
+                                            {vinyl.name}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <Link
-                                to={`/vinyls/${vinyl.slug}`}
-                                className="btn btn-outline-dark w-100 mt-2">
-                                View details
-                            </Link>
+                                <Link
+                                    to={`/vinyls/${vinyl.slug}`}
+                                    className="btn btn-outline-dark w-100 mt-2">
+                                    View details
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
+
     )
 }
