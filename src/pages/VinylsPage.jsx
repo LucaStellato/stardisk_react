ee3d64ae4934e24a0aa3f64f5950a103e3ab9a51
LucaStellato <lucastellato435@gmail.com>
2026-01-15 13:35:18 +0100

    add serachbar

--- src/pages/VinylsPage.jsx
+++ src/pages/VinylsPage.jsx
@@ -5,7 +5,10 @@ import CatalogBox from "../components/CatalogBox"
 export default function VinylsPage() {
 
     const [vinyls, setVinyls] = useState([])
+    const [query, setQuery] = useState("")
+    const [results, setResults] = useState([]
 
+    )
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then(response => {
@@ -18,14 +21,33 @@ export default function VinylsPage() {
             })
     }, [])
 
+    const handleSearch = () => {
+        fetch(`http://localhost:3000/api/products/search?q=${encodeURIComponent(query)}`)
+            .then((res) => res.json())
+            .then((data) => setResults(data))
+        console.log(results)
+    }
+
+
     return (
         <div className="bg-lightyellow schizzi">
             <div className="container py-5">
                 <h1 className="fw-bold text-blue">VINYL CATALOG</h1>
+                <div>
+                    <input type="text" placeholder="type to search..." value={query} onChange={(e) => setQuery(e.target.value)} style={{ width: '30%', padding: '8px' }} />
+                    <button type="button" onClick={handleSearch} style={{ marginTop: '10px' }} className="p-2 text-blue">Search</button>
+                </div>
                 <div className="row">
                     {vinyls.map(vinyl => (
                         <div className="col-12 col-md-6 col-xl-4" key={vinyl.slug}>
                             <CatalogBox vinyl={vinyl} />
+                        </div>
+                    ))}
+                </div>
+                <div className="row">
+                    {results.map(result => (
+                        <div className="col-12 col-md-6 col-xl-4" key={result.slug}>
+                            <CatalogBox vinyl={result} />
                         </div>
                     ))}
                 </div>
